151fccf66ab798cd556f6f89407e3a398e8495c5
Executioner47 <faresgalal09@gmail.com>
2022-10-09 18:21:32 +0200

    filter component created

---
+++ FrontEnd/src/app/Components/filter-products/filter-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-products',
+  templateUrl: './filter-products.component.html',
+  styleUrls: ['./filter-products.component.css']
+})
+export class FilterProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
