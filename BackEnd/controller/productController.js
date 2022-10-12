1c2c01e3d5fc62f2ebbf2cf44ba526f2870a58e9
Executioner47 <faresgalal09@gmail.com>
2022-10-12 12:24:43 +0200

    remove unused functions

--- BackEnd/controller/productController.js
+++ BackEnd/controller/productController.js
@@ -2,7 +2,6 @@ let Product = require("../model/product");
 
 exports.getAllProducts = async (req, res) => {
   let category = req.query.category;
-
   let products;
   try {
     if (category) products = await Product.where({ category: category });
@@ -34,85 +33,3 @@ exports.getProductById = async (req, res, next) => {
   });
 };
 
-exports.getProduct = async (req, res) => {
-  let category = req.query.cat;
-  let brand = req.query.brand;
-  let products;
-  if (brand) {
-    try {
-      products = await Product.find({ category: category });
-    } catch (err) {
-      console.log(err);
-    }
-    if (!products) {
-      return res.status(404).json({ message: "No products found !" });
-    }
-
-    let uniqueArr = new Set();
-    products.map((item) => {
-      uniqueArr.add(item.name.split(" ")[0]);
-    });
-
-    let choosenBrandProducts = new Array();
-    products.map((item) => {
-      if (item.name.split(" ")[0].toLowerCase() === brand.toLowerCase()) {
-        choosenBrandProducts.push(item);
-      }
-    });
-
-    return res.status(201).render(`viewAllProduct`, {
-      title: "Products",
-      user: req.user,
-      choosenBrandProducts,
-      cat: category,
-      uniqueArr,
-    });
-  } else {
-    let category = req.query.cat;
-    let products;
-    try {
-      products = await Product.find({ category: category });
-    } catch (err) {
-      console.log(err);
-    }
-    if (!products) {
-      return res.status(404).json({ message: "No products found !" });
-    }
-
-    let uniqueArr = new Set();
-    products.map((item) => {
-      uniqueArr.add(item.name.split(" ")[0]);
-    });
-
-    return res.status(201).render(`viewAllProduct`, {
-      title: "Products",
-      user: req.user,
-      arr: products,
-      cat: category,
-      uniqueArr,
-    });
-  }
-};
-
-exports.addProduct = async (req, res, next) => {
-  const name = req.body.name;
-  const category = req.body.category;
-  const image = req.body.image;
-  const price = req.body.price;
-  let product;
-  try {
-    product = new Product({
-      name: name,
-      category: category,
-      price: price,
-      image: image,
-    });
-    await product.save();
-  } catch (err) {
-    console.log(err);
-  }
-  if (!product) {
-    return res.status(500).json({ message: "unable To add" });
-  }
-  return res.status(201).json({ product });
-};
